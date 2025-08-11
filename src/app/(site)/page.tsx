dc7993c7bd88d4dc05d13b03b72fa5c1cc9e4892
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-08-12 00:29:26 +0530

    landing page hero section

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
-
+import TitleSection from "@/components/landingPage/titleSection";
+import { Button } from "@/components/ui/button";
+import React from "react";
+import AppBanner from "../../../public/appBanner.png";
+import Image from "next/image";
 function page() {
   return (
-    <div>
-        wsn 
-    </div>
-  )
+    <>
+      <section className="overflow-hidden px-4 sm:px-6 mt-10  sm:flex  sm:flex-col gap-4 md:justify-center md:items-center">
+        <TitleSection
+          pill="✨ Your Ideas, Organized"
+          title="The All-in-One Workspace for Teams and Creators"
+        />
+        <div className="p-[2px] bg-white mt-6 rounded-xl bg-gradient-to-r from-primary to-brand-primary-blue sm:w-[300px]">
+          <Button
+            variant={"secondary"}
+            className="w-full rounded-[13px] p-6 text-2xl bg-background"
+          >
+            Get Arcane Free
+          </Button>
+        </div>
+        <div className="md:mt-[-90px] sm:w-full w-[750px] flex justify-center items-center mt-[-40px] relative sm:ml-0 ml-[-50px]">
+          <Image
+            src={AppBanner}
+            alt="App Banner"
+            className="mask-b-from-35%"
+          ></Image>
+        </div>
+      </section>
+      <section className="relative">
+        <div className="overflow-hidden flex">
+
+        </div>
+
+      </section>
+    </>
+  );
 }
 
-export default page
+export default page;
