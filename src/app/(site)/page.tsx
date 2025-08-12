9c2f157b85c8e71c6623b48db7a4de63ea5568df
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-08-12 19:58:20 +0530

    img sliders

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 import AppBanner from "../../../public/appBanner.png";
 import Image from "next/image";
+import { CLIENTS } from "@/lib/CONSTANT";
 function page() {
   return (
     <>
@@ -28,10 +29,29 @@ function page() {
         </div>
       </section>
       <section className="relative">
-        <div className="overflow-hidden flex">
+        <div className="overflow-hidden flex after:content-[''] after:dark:from-brand-dark after:to-transparent after:from-background after:bg-gradient-to-l after:right-0 after:bottom-0 after:top-0 after:w-20 after:absolute after:z-10  before:content-[''] before:dark:from-brand-dark before:to-transparent before:from-background before:bg-gradient-to-r before:left-0 before:bottom-0 before:w-20 before:top-0  before:z-10 before:absolute">
+
+          {[...Array(6)].map((_, index) => (
+            <div
+              key={index}
+              className="flex flex-nowrap animate-slide"
+            >
+              {CLIENTS.map((client, idx) => (
+                <div key={idx} className="relative w-[200px] m-20 shrink-0 flex items-center">
+                  <Image
+                    src={client.logo}
+                    alt={client.alt}
+                    width={200}
+                    className=" object-contain max-w-none"
+                  />
+
+                  </div>
+              ))}
+              
+            </div>
+          ))}
 
         </div>
-
       </section>
     </>
   );
