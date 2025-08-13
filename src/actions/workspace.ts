cb0c34e26062be5b38e48f2808ffc7f34c6b9bfc
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2025-08-14 01:00:55 +0530

    w-setup

--- src/actions/workspace.ts
+++ src/actions/workspace.ts
@@ -4,22 +4,26 @@ import { User } from "@supabase/supabase-js";
 import { prisma } from "@/lib/supabase/db";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { createClient } from "@/lib/supabase/server";
 
 export async function createWorkspace(
   formData: FormData,
   user: User,
   selectedEmoji: string
 ) {
+  const supabase = createClient();
   const workspaceName = formData.get("workspace-name") as string;
   const workspaceLogo = formData.get("workspace-logo") as any;
   const workspaceId = crypto.randomUUID();
+  let imageUrl = null;
+  
+
   const workspace = await prisma.workSpace.create({
     data: {
       id: workspaceId,
       title: workspaceName,
       workSpaceOwner: user.id,
       iconId: selectedEmoji,
-      logoUrl: workspaceLogo,
     }
   });
   revalidatePath("/");
